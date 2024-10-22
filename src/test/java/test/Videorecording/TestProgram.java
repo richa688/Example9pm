df044041a2c2c0b410f8931f22e46041d2cd5ebf
richa688 <task.stadsolution@gmail.com>
2024-10-22 21:26:36 +0530

    seven commit

--- src/test/java/test/Videorecording/TestProgram.java
+++ src/test/java/test/Videorecording/TestProgram.java
@@ -5,7 +5,6 @@ public class TestProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello this is testProgram file");
-		System.out.println("This is new line");
 
 	}
 
